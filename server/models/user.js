2ad81607fbe6423938e3669386ab723df31292ab
datnguyen <dat.nguyen@redharegames.com>
2018-05-25 21:37:49 +0700

    update gitignore & validator email

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,16 @@
-var mongoose = require('mongoose');
-
+const mongoose = require('mongoose');
+const validator = require('validator');
 var User = mongoose.model('User', {
     email: {
         type: String,
         required: true,
         trim: true,
         minlength: 6,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
     },
     phoneNumber: {
         type: String,
